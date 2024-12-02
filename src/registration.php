bd0859b360ae5ebbe2c11e56b5ce6121cb7ebe19
Jeanmarcos Juarez <janmarcoj@gmail.com>
2024-12-02 08:47:01 -0400

    Initial Commit

---
+++ src/registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the MageObsidian - ModernFrontend project.
+ *
+ * @license MIT License - See the LICENSE file in the root directory for details.
+ * © 2024 Jeanmarcos Juarez
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'MageObsidian_ModernFrontendCli',
+    __DIR__
+);
+
